5ba2bde672ab7fe5a0ce81cd1204a60f4e90db81
Christian Schöpp <mail@dadadesign.de>
2020-07-04 09:08:59 +0200

    Remove obsolete code

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,3 @@
-(function($){
-/*
-// main.js
-// This code is free to use or distribute.
-// Copyright (C) 2018-2019 by Christian Schoepp
-*/
-
-// Polyfill the window objects
-// 'requestAnimationFrame' and
-// 'cancelAnimationFrame'
-
 var animationFrameTimeout;
 var requestAnimationFrame =
 window.requestAnimationFrame ||
@@ -26,84 +15,7 @@ window.mozCancelAnimationFrame ||
 clearTimeout(animationFrameTimeout);
 window.cancelAnimationFrame = cancelAnimationFrame;
 
-var MAIN = (function() {
-    // Main project module
-    // provides helper methods
-    // and project globals
+(function($){
 
-    // Project globals
-    var
-    $body = $('body'),
-    is_desktop,
-    scrollPosition,
-    windowHeight,
-    windowWidth,
-    init = function() {
-        scrollPosition = $(document).scrollTop();
-        windowHeight = $(window).height();
-        windowWidth = $(window).width();
-        is_desktop = ($(window).outerWidth()>768) ? true : false;
-        $(window).scroll(scroller);
-        $(window).resize(resizer);
-    },
-    // Helper methods
-    info = function(a){
-        console.log(a)
-    },
-    rand = function(a,b,c){
-        a = ( ( Math.random() * (b-a) ) + a );
-        a = c ? Math.floor(a) : a;
-        return a
-    },
-    loop = function(num,callback){
-        var n = num;
-        for( var i = 0; i < n; i++ ){
-            callback(i)
-        }
-    },
-    scroller = function(){
-        scrollPosition = $(document).scrollTop();
-    },
-    resizer = function(){
-        windowHeight = $(window).height();
-        is_desktop = ($(window).outerWidth()<=768) ? false : true;
-    }
-
-    init()
-
-    // Export all
-    return {
-        info: info,
-        rand: rand,
-        loop: loop,
-        $body: $body,
-        scrollPosition: function(){return scrollPosition},
-        windowHeight: function(){return windowHeight},
-        windowWidth: function(){return windowWidth},
-        is_desktop: function(){return is_desktop}
-    }
-
-})();//MAIN
-
-var FEATURE01 = (function (m) {
-    // Quick import all from MAIN
-    for( g in m ){ this[g] = m[g] }
-
-    var
-    init = function(){
-        some()
-    },
-    some = function(){
-        
-    }
-
-    return{
-        init: init
-    }
-
-})(MAIN)//FEATURE01
-
-//INITIALIZE
-FEATURE01.init();
 
 })(jQuery)//End of File
